10ff1df55688160d15fa066297274a3cf4100495
Abul Kalam Azad <azadsmc@gmail.com>
2022-10-27 17:05:35 +0600

    Install ckEditor and customize product form

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use dosamigos\ckeditor\CKEditor;
 use yii\helpers\Html;
 use yii\bootstrap5\ActiveForm;
 
@@ -14,13 +15,29 @@ use yii\bootstrap5\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'description')->widget(CKEditor::className(), [
+        'options' => ['rows' => 6],
+        'preset' => 'basic'
+    ])  ?>
 
-    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image',[
+            'template' => '
+                <div class="custom-file">
+                    {input}
+                    {label}
+                    {error}
+                </div>
+            ',
+        'inputOptions' => ['class'=> 'custom-file-input'],
+        'labelOptions' => ['class'=> 'custom-file-label']
+    ])->fileInput(['type' => 'file']) ?>
 
-    <?= $form->field($model, 'price')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'price')->textInput([
+            'maxlength' => true,
+            'type' => 'number'
+    ]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'status')->checkbox() ?>
 
     <?= $form->field($model, 'created_at')->textInput() ?>
 
@@ -31,7 +48,9 @@ use yii\bootstrap5\ActiveForm;
     <?= $form->field($model, 'updated_by')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Save', [
+                'class' => 'btn btn-success'
+        ]) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
